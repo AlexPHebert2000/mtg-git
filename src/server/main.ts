e9b29b4251191d88f5fed67252aae073e4fd46f6
Alex Hebert <alex.p.hebert.dev@gmail.com>
2025-04-09 14:30:09 -0500

    ADD body parser

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import router from "./router.js";
+import bodyParser from "body-parser";
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use("/api", router);
 
 ViteExpress.listen(app, 3000, () =>
